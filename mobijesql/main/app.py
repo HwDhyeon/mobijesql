8a8bd527121d12af8a5520bff4d0efbeb37aae85
HwDhyeon <youfsk@naver.com>
2021-05-24 17:04:22 +0900

    Add create_build and create_e2e_test

--- mobijesql/main/app.py
+++ mobijesql/main/app.py
@@ -21,9 +21,15 @@ def read_builds(skip: int = 0, limit: int = 100, db: Session = a_db):
 
 
 def create_build(build: schemas.build.BuildCreate, db: Session = a_db):
-    db_build = crud.get_build_by_info(db, build_name=build.name, build_number=build.number)
+    db_build = crud.get_build_by_info(
+        db,
+        build_name=build.name,
+        build_number=build.number
+    )
     if db_build:
-        raise DataDuplicatedError(f'{build.name}의 {build.number}번 빌드는 이미 존재합니다.')
+        raise DataDuplicatedError(
+            f'{build.name}의 {build.number}번 빌드는 이미 존재합니다.'
+        )
     return crud.create_build(db=db, build=build)
 
 
@@ -34,8 +40,11 @@ def read_e2e_tests(skip: int = 0, limit: int = 100, db: Session = a_db):
     return tests
 
 
-def create_e2e_test(e2etest: schemas.e2etest.E2EtestCreate, db: Session = a_db):
-        db_e2etest = crud.get_e2e_test_by_build(db, e2etest.build_id)
-        if db_e2etest and db_e2etest.stage == e2etest.stage:
-            raise DataDuplicatedError(f'{build.name}의 {build.number}번 빌드는 이미 존재합니다.')
-        return crud.create_e2e_test(db, e2etest=e2etest)
+def create_e2e_test(
+    e2etest: schemas.e2etest.E2EtestCreate,
+    db: Session = a_db
+):
+    db_e2etest = crud.get_e2e_test_by_build(db, e2etest.build_id)
+    if db_e2etest and db_e2etest.stage == e2etest.stage:
+        raise DataDuplicatedError(f'{e2etest.stage} 스테이지는 이미 존재합니다.')
+    return crud.create_e2e_test(db, e2etest=e2etest)
